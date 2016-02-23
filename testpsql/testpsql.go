ea59cd1025e1a99de1ef83440bd92daf51fc6663
Leonardo Castro <leonardo_castro@ciencias.unam.mx>
2016-02-23 00:14:09 -0600

    Added sql.NullString

--- testpsql/testpsql.go
+++ testpsql/testpsql.go
@@ -4,20 +4,21 @@ import( "github.com/jmoiron/sqlx"
         _ "github.com/lib/pq"
         "log"
         "fmt"
+        "database/sql"
       )
 
  var schema = `
  CREATE TABLE closet2(
    id int,
-   kind text,
+   type text,
    branch text,
    color text
    )`
 
 type Closet struct {
   Id int
-  Kind string
-  Branch string
+  Type string
+  Branch sql.NullString
   Color string
 }
 
@@ -31,19 +32,33 @@ func main() {
 
   // en testdb ya tengo una TABLA llamada closet.
   // Primero coloquemos algo en closet 2.
+  db.MustExec(schema)
 
-  db.MustExec("INSERT INTO closet2 (id, kind, branch, color) VALUES ($1, $2, $3, $4)", 1, "sweater", "DEFAULT", "blue")
+  db.MustExec("INSERT INTO closet2 (id, type, branch, color) VALUES ($1, $2, $3, $4)", 1, "sweater", "DEFAULT", "blue")
 
   // Mostrémoslo
   closet2 := []Closet{}
-  db.Select(&closet2, "SELECT * FROM closet2")
+  err = db.Select(&closet2, "SELECT * FROM closet2")
+  if err != nil {
+      log.Println(err)
+    }
   fmt.Println(closet2[0])
 
 
   // Ahora leamos la TABLA ya existente
   closet := []Closet{}
-  db.Select(&closet, "SELECT * FROM closet")
+  err = db.Select(&closet, "SELECT * FROM closet")
+  if err != nil {
+    log.Println(err)
+  }
+
+  // Para poder observar los valores en la tabla:
    for _, c := range closet {
      fmt.Println(c)
+   }
+
+   // La columna branch puede tener valores NULL, por lo que hay que hacer entonces:
+   for _, c := range closet {
+     fmt.Println(c.Branch.String)
    }
 }
