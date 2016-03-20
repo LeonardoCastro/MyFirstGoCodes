5f6c7c83feee0b3d9820d60f94df27c523400913
Leonardo Castro <leonardo_castro@ciencias.unam.mx>
2016-03-19 18:44:30 -0600

    reorganizing

--- password/samelength.go
+++ password/samelength.go
@@ -9,47 +9,96 @@ import (
 	"time"
 )
 
-// Longer returns a passphrase when the desired length is smaller than the phrase's length.
-func Longer(p Passphrase, m int) string {
-	p.Phrase = WordsForSym(p.Phrase)
-	n := len(p.Phrase)
+// SameLengths returns the passphrase when the length of the phrase is the same as the desired length for the passphrase
+func SameLengths(p Passphrase, n int) string {
 	password := p.Phrase
-	switch {
-	case n < m:
-		password = Shorter(p, m, n)
-	case n == m:
-		password = SameLengths(p, n)
-	case n > m:
-		password = Shortening(p, m, n)
-	}
-	return password
-}
-
-// Shortening erases chars from the user's phrase until the desired length is obtained.
-func Shortening(p Passphrase, m, n int) string {
-	seed := time.Now().UTC().UnixNano()
-	rand.Seed(seed)
-
-	arrayPassword := strings.Split(p.Phrase, "")
 	s1 := p.PersonalInfo1
 	s2 := p.PersonalInfo2
 
-	idx1 := strings.Index(p.Phrase, s1)
-	idx2 := strings.Index(p.Phrase, s2)
+	ns1 := len(s1)
+	ns2 := len(s2)
 
-	changes := 0
-	for changes < n-m+1 {
-		if i := rand.Intn(n); (i < idx1 || i >= idx1+len(s1)) && (i < idx2 || i >= idx2+len(s2)) {
-			arrayPassword[i] = ""
-			changes++
-		}
+	// Quitamos s2 de donde esté
+	if strings.Contains(password, s2) {
+		password = strings.Replace(password, s2, "", -1)
 	}
-	fmt.Println(arrayPassword)
-	p.Phrase = strings.Join(arrayPassword, "")
-	password := SameLengths(p, n)
 
+	// Quitamos s2 de donde esté
+	if strings.Contains(password, s1) {
+		password = strings.Replace(password, s1, "", -1)
+	}
+
+	// arrayPassword := strings.Split(password, "")
+	fmt.Println(password)
+	arrayS1 := strings.Split(s1, "")
+	arrayS2 := strings.Split(s2, "")
+
+	paso := ns2/ns1 + 1
+
+	switch {
+	case ns1 >= ns2:
+		password = ProcessSameLength(arrayS1, arrayS2, password, ns2, ns1, paso)
+	case ns2 > ns1:
+		password = ProcessSameLength(arrayS2, arrayS1, password, ns2, ns1, paso)
+	}
 	return password
 }
+
+// ProcessSameLength distributes s1 and s2 randomly into the password
+func ProcessSameLength(arrayS1, arrayS2 []string, password string, ns2, ns1, paso int) string {
+
+	seed := time.Now().UTC().UnixNano()
+	rand.Seed(seed)
+
+	var cuenta1, cuenta2, L int
+
+	for L < ns1+ns2 {
+
+		s := rand.Intn(2)
+
+		if s == 0 {
+			password, cuenta1 = Fill(password, arrayS1, cuenta1)
+		} else {
+			password, cuenta2 = Fill(password, arrayS2, cuenta2)
+		}
+		L = cuenta1 + cuenta2
+	}
+
+	arrayPassword := No3strings(strings.Split(password, ""))
+
+	return strings.Join(arrayPassword, "")
+}
+
+// Fill fills password with personal info
+func Fill(password string, array []string, cuenta int) (string, int) {
+
+	if cuenta < len(array) {
+		j := rand.Intn(len(password) + 1)
+		password = password[:j] + array[cuenta] + password[j:]
+		cuenta++
+	}
+	return password, cuenta
+}
+
+// No3strings verifies that there is no three consecutive strings.
+func No3strings(arrayPassword []string) []string {
+
+	seed := time.Now().UTC().UnixNano()
+	rand.Seed(seed)
+
+	for i, s := range arrayPassword[:len(arrayPassword)-3] {
+		if ok, _ := regexp.MatchString("[A-Za-z]", s); ok {
+			if ok, _ := regexp.MatchString("[A-Za-z]", arrayPassword[i+1]); ok {
+				if ok, _ := regexp.MatchString("[A-Za-z]", arrayPassword[i+2]); ok {
+					arrayPassword[i+1] = strconv.Itoa(rand.Intn(10))
+				}
+			}
+		}
+	}
+	return arrayPassword
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 // FindPassword method to found safe passwords
 func FindPassword(p Passphrase) string {
@@ -184,15 +233,4 @@ func CountingZeros(array []int, idx1, idx2 int) int {
 		}
 	}
 	return count
-}
-
-// WordsForSym changes words "And" or "Or" for symbols
-func WordsForSym(password string) string {
-	password = strings.Replace(password, "And", "&", 1)
-	password = strings.Replace(password, "and", "&", 1)
-
-	password = strings.Replace(password, "Or", "?", 1)
-	password = strings.Replace(password, "or", "?", 1)
-
-	return password
 }
